f105c3ae1c2dd03bfcddce9aa32c8261bd6b8dbd
AtiladDias <128435253+AtiladDias@users.noreply.github.com>
2026-04-06 13:29:25 -0300

    Add MobilePage component with PhoneFrame and DashboardScreen

---
+++ app/   src/mobile/page.tsx
@@ -0,0 +1,10 @@
+import PhoneFrame from "@/components/mobile/PhoneFrame";
+import DashboardScreen from "@/components/mobile/DashboardScreen";
+
+export default function MobilePage() {
+  return (
+    <PhoneFrame>
+      <DashboardScreen />
+    </PhoneFrame>
+  );
+}
